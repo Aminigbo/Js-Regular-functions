796510c9cfa4970af2c2833e3bfb78a7718585e6
Aminigbo <61628021+Aminigbo@users.noreply.github.com>
2023-12-05 12:13:53 +0100

    added handleInputChange

--- filterInputOnChange.js
+++ filterInputOnChange.js
@@ -1,1 +1,8 @@
-
+const handleInputChange = (text) => {
+        const filteredSuggestions = Cooks.filter(
+            state => state.vendorName.toLowerCase().startsWith(text.toLowerCase())
+        );
+        
+        // setSearch(text); // save typed characters to state
+        // setSuggestions(filteredSuggestions); // save filtered data to state
+    };
