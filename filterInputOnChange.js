070d320de436b4a709edf41c2313788b30942c93
Aminigbo <61628021+Aminigbo@users.noreply.github.com>
2023-12-05 12:17:01 +0100

    added filteredSuggestionsByAllCharacter

--- filterInputOnChange.js
+++ filterInputOnChange.js
@@ -1,3 +1,4 @@
+// ======= Search the array for words starting with the typed character
 const handleInputChange = (text) => {
         const filteredSuggestions = Cooks.filter(
             state => state.vendorName.toLowerCase().startsWith(text.toLowerCase())
@@ -6,3 +7,14 @@ const handleInputChange = (text) => {
         // setSearch(text); // save typed characters to state
         // setSuggestions(filteredSuggestions); // save filtered data to state
     };
+
+
+// =======search the array for words that contain the typed character
+  const filteredSuggestionsByAllCharacter = NigerianStates.filter(
+      state =>
+        state.name.toLowerCase().includes(text.toLowerCase()) ||
+        state.capital.toLowerCase().includes(text.toLowerCase())
+    );
+    // setSearch(text); // save typed characters to state
+        // setSuggestions(filteredSuggestions); // save filtered data to state
+  };
